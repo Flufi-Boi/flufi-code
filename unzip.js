151ae58ab3842e6c7f089e7adf025a9e786d1f4a
Flufi-boi <floofster@flufi.gay>
2025-12-22 19:52:18 +0000

    uploaded fluficode and zippers

---
+++ unzip.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+const path = require("path");
+
+const zip = JSON.parse(fs.readFileSync("fluficode.json", "utf-8"));
+
+function unzipFile(f, p) {
+    if (!fs.existsSync(p))
+        fs.mkdirSync(p);
+    console.log(path.join(p, f[1] + f[0]));
+    if (f[0] == ".folder") {
+        fs.mkdirSync(path.join(p, f[1]));
+        for (let i = 0; i < f[2].length; i++) {
+            unzipFile(f[2][i], path.join(p, f[1]));
+        }
+    } else {
+        fs.writeFileSync(path.join(p,`${f[1]}${f[0]}`), f[2]);
+    }
+}
+
+fs.rmSync("./fluficode", { recursive: true });
+
+unzipFile(zip, "./");
